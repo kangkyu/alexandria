561d90da27606459e9cfcfe7103d78ea653a35b5
Kang-Kyu Lee <kangkyu1111@gmail.com>
2016-09-30 16:10:15 -0700

    Add Book model spec and its pass

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :publisher
   belongs_to :author
+
+  validates :title, :isbn_10, :isbn_13, :released_on, :author, presence: true
+  validates :isbn_10, length: { is: 10 }
+  validates :isbn_13, length: { is: 13 }
+  validates :isbn_10, uniqueness: true
+  validates :isbn_13, uniqueness: true
 end
--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,13 +1,12 @@
 FactoryGirl.define do
   factory :book do
-    title "MyString"
-subtitle "MyString"
-isbn_10 "MyString"
-isbn_13 "MyString"
-description "MyText"
-released_on "2016-09-30"
-publisher nil
-author nil
+    title "Master Ruby Web APIs"
+    subtitle "MyString"
+    isbn_10 "0123456789"
+    isbn_13 "0123456789012"
+    description "MyText"
+    released_on "2016-09-30"
+    publisher
+    author
   end
-
 end
