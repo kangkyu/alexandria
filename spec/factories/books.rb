4ad884f9bd00d5a0a3470fbbb5002dda7e350378
Kang-Kyu Lee <kangkyu1111@gmail.com>
2016-09-30 15:43:11 -0700

    rails g model Book title subtitle isbn_10 isbn_13 description:text ...
    released_on:date publisher:references author:references
    
      invoke  active_record
      create    db/migrate/20160930224214_create_books.rb
      create    app/models/book.rb
      invoke    rspec
      create      spec/models/book_spec.rb
      invoke      factory_girl
      create        spec/factories/books.rb
    
    add extra unique and index

---
+++ spec/factories/books.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :book do
+    title "MyString"
+subtitle "MyString"
+isbn_10 "MyString"
+isbn_13 "MyString"
+description "MyText"
+released_on "2016-09-30"
+publisher nil
+author nil
+  end
+
+end
