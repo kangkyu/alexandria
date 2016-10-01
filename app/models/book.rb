ccccc1f5ea9f1a753980b88608a5f603d6702410
Kang-Kyu Lee <kangkyu1111@gmail.com>
2016-09-30 17:00:20 -0700

    Chapter 14 - start with books#index action

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,9 +2,7 @@ class Book < ApplicationRecord
   belongs_to :publisher, required: false
   belongs_to :author
 
-  validates :title, :isbn_10, :isbn_13, :released_on, :author, presence: true
-  validates :isbn_10, length: { is: 10 }
-  validates :isbn_13, length: { is: 13 }
-  validates :isbn_10, uniqueness: true
-  validates :isbn_13, uniqueness: true
+  validates :title, :released_on, :author, presence: true
+  validates :isbn_10, presence: true, length: { is: 10 }, uniqueness: true
+  validates :isbn_13, presence: true, length: { is: 13 }, uniqueness: true
 end
---
+++ spec/requests/books_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Books", type: :request do
+
+  describe 'GET /api/books' do
+  end
+end
