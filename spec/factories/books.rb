837b854707c44f41b1941026b7735b94402d0b88
Kang-Kyu Lee <kangkyu1111@gmail.com>
2016-09-30 17:21:10 -0700

    exposing the books resource in test env

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -6,6 +6,39 @@ FactoryGirl.define do
     isbn_13 "0123456789012"
     description "MyText"
     released_on "2016-09-30"
+    publisher nil
+    author
+  end
+
+  factory :ruby_microscope, class: Book do
+    title "Ruby Under a Microscope"
+    subtitle "MyString"
+    isbn_10 "1123456789"
+    isbn_13 "1123456789012"
+    description "MyText"
+    released_on "2016-09-30"
+    publisher
+    author
+  end
+
+  factory :ruby_on_rails_tutorial, class: Book do
+    title "Ruby on Rails Tutorial"
+    subtitle "MyString"
+    isbn_10 "2123456789"
+    isbn_13 "2123456789012"
+    description "MyText"
+    released_on "2016-09-30"
+    publisher
+    author
+  end
+
+  factory :agile_web_development, class: Book do
+    title "Agile Web Development with Rails 4"
+    subtitle "MyString"
+    isbn_10 "3123456789"
+    isbn_13 "3123456789012"
+    description "MyText"
+    released_on "2016-09-30"
     publisher
     author
   end
